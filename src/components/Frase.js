9631b51f3f01bd330e9d390e26966fc8a81170b5
Jânderson Machado <jandersonmachado090@gmail.com>
2023-06-19 11:16:09 -0300

    add alterações

---
+++ src/components/Frase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Frase() {
+    return (
+        <div>
+            <p>Este é o componente com uma frase</p>
+        </div>
+    )
+}
+
+export default Frase
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Frase from './Frase'
 
 const HelloWorld = () => {
-    return(
+    return (
         <div>
+            <Frase />
             <h1>Hello World</h1>
             <h2>Hello World</h2>
         </div>
