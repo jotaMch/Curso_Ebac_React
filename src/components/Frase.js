8f46133d07fe592aba54b20fe6ecdc9d3eff0b96
Jânderson Machado <jandersonmachado090@gmail.com>
2023-06-19 18:50:39 -0300

    aplicando o extends a classe Frase

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
-function Frase(props) {
+class Frase extends React.Component {
+    render() {
     return (
         <div>
             <div className="hora">
-                <h5>{props.name} World</h5>
+                <h5>{this.props.name} World</h5>
                 <p> as 17:13 de 19/06/2023</p>
             </div>
             <div className="hora">
-                <h2>{props.name} World</h2>
+                <h2>{this.props.name} World</h2>
                 <p> as 17:13 de 19/06/2023</p>                                          
             </div>
             <div className="hora">
-                <button>{props.name} World</button>
+                <button>{this.props.name} World</button>
                 <p> as 17:07 de 19/06/2023</p>
             </div>
             <div className="hora">
-                <li>{props.name} World</li>
+                <li>{this.props.name} World</li>
                 <p> as 17:14 de 19/06/2023</p>
             </div>
         </div>
     )
 }
+}
+
 
 export default Frase
