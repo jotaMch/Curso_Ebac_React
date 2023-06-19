72c2da2dfc4d96ae85007335b42c5478a5290207
Jânderson Machado <jandersonmachado090@gmail.com>
2023-06-19 10:38:21 -0300

    alteração no conteudo app e helloworld

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => {
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <h2>Hello World</h2>
+        </div>
+    )
+}
+
+export default HelloWorld
